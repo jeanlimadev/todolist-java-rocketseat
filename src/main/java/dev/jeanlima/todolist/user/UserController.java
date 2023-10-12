4cba5ae8d592c61d436e8a496af46e695edd60f5
Jean Lima <jeanlima.dev@gmail.com>
2023-10-11 23:49:06 -0300

    aula 3

--- src/main/java/dev/jeanlima/todolist/user/UserController.java
+++ src/main/java/dev/jeanlima/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,7 +24,12 @@ public class UserController {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Já existe!");
     }
 
+    var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(hashedPassword);
+
     var userCreated = this.userRepository.save(userModel);
+    
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
